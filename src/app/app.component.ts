af348abddc1f05f50d229a68cf6e682ddfd28dd8
Magdalena Zdybel <magda@motta.com.pl>
2019-01-27 20:26:25 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-mine',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'my-first-project-in-Angular';
+
+  getAge(){
+    return 2019 - this.mine.age;
+  }
+
+  mine= {
+    name: 'Magda',
+    age: 1978,
+    company:{
+      name: 'Magda Zdybel'
+    }
+  }
 }
